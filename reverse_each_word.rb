c355cb5c7db3b9fbfc2e671e6011ed0738dc8433
Greg Morgan <gregory.morgan@flatironschool.com>
2019-04-16 13:27:36 -0700

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,3 @@
-# def reverse_each_word(str)
-#     newAr=[]
-#     str.split(" ").each do |word|
-#         newAr.push(word.reverse)
-#     end
-#     newAr.join(" ")
-# end
-
 def reverse_each_word(str)
     str.split(" ").collect { |word| word.reverse}.join(" ")
 end
